44cb8a7c65fa6b30a3a61da82bf52b95545870ad
Yelnur <elnur.sul06@gmail.com>
2026-04-20 22:32:33 +0500

    Adding login to path

--- frontend/SmartPath/src/app/app.routes.ts
+++ frontend/SmartPath/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { LandingPage } from './pages/landing/landing';
 import { SchedulePage } from './pages/schedule/schedule';
 import {NotificationsPage} from './pages/notifications/notifications';
+import { LoginPage } from './pages/login/login';
+import { guestGuard, authGuard} from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: LandingPage },
-  { path: 'schedule', component: SchedulePage },
-  { path: 'notifications', component: NotificationsPage },
+  { path: 'schedule', component: SchedulePage, canActivate: [authGuard] },
+  { path: 'notifications', component: NotificationsPage, canActivate: [authGuard] },
+  { path: 'login', component: LoginPage, canActivate: [guestGuard] },
   { path: '**', redirectTo: '' },
 ];
