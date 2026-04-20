b99d2384d93026ef7a3f58d7128df44e5f5acee8
kozhamuratoval <lianakozamuratova@gmail.com>
2026-04-21 03:38:40 +0500

    cleaned some trash unused

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,9 +1,7 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from django.utils import timezone
-
 from .models import Task, ScheduleEntry
 from .serializers import TaskSerializer, ScheduleEntrySerializer
 
